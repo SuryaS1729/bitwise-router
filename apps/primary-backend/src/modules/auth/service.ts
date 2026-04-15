ccf6d6c36ca5414ba4a3ca65a3522510bd249165
SuryaS1729 <143543633+SuryaS1729@users.noreply.github.com>
2026-04-15 18:27:40 +0530

    turborepo init routine

--- apps/primary-backend/src/modules/auth/service.ts
+++ apps/primary-backend/src/modules/auth/service.ts
@@ -1,14 +1,11 @@
-export abstract class AuthService{
+import { prisma } from "db";
 
-    static async signup(username:string, password:string):Promise<string>{
-        return "123"
+export abstract class AuthService {
+  static async signup(username: string, password: string): Promise<string> {
+    return "123";
+  }
 
-    }
-
-    static async signin(username:string, password:string):Promise<string>{
-        return "token: asfdasfkldjalksdfj"
-
-    }
-
-
+  static async signin(username: string, password: string): Promise<string> {
+    return "token: asfdasfkldjalksdfj";
+  }
 }
