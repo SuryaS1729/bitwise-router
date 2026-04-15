7ba5a21bcd5b5a02048b6f818439377e81c2310f
SuryaS1729 <143543633+SuryaS1729@users.noreply.github.com>
2026-04-15 19:00:19 +0530

    db calls work

--- apps/primary-backend/src/modules/auth/index.ts
+++ apps/primary-backend/src/modules/auth/index.ts
@@ -1,32 +1,45 @@
-import {Elysia} from 'elysia';
-import { AuthModel } from './models';
-import { AuthService } from './service';
+import { Elysia } from "elysia";
+import { AuthModel } from "./models";
+import { AuthService } from "./service";
+import { prisma } from "db";
 
-export const app = new Elysia({prefix: "auth"})
-.post("/sign-up", async ({body})=>{
-    const userId = await AuthService.signup(body.email,body.password);
-    return {
-        id:userId
-    }
-
-
-},{
-    body:AuthModel.signUpSchema,
-    response:{
+export const app = new Elysia({ prefix: "auth" })
+  .post(
+    "/sign-up",
+    async ({ body, status }) => {
+      try {
+        const userId = await AuthService.signup(body.email, body.password);
+        return {
+          id: userId,
+        };
+      } catch (e) {
+        console.log(e);
+        return status(400, {
+          message: "error while signing up",
+        });
+      }
+    },
+    {
+      body: AuthModel.signUpSchema,
+      response: {
         200: AuthModel.signUpResponseSchema,
-        400:AuthModel.signUpFailedResponseSchema
-    }
-})
-.post("/sign-in", async ({body})=>{
-    const userToken = await AuthService.signin(body.email,body.password);
-    return{
-        token: userToken
-    }
-
-},{
-    body: AuthModel.signInSchema,
-    response:{
-        200:AuthModel.signInResponseSchema,
-        400:AuthModel.signInFailedResponseSchema
-    }
-})
+        400: AuthModel.signUpFailedResponseSchema,
+      },
+    },
+  )
+  .post(
+    "/sign-in",
+    async ({ body }) => {
+      const userToken = await AuthService.signin(body.email, body.password);
+      return {
+        token: userToken,
+      };
+    },
+    {
+      body: AuthModel.signInSchema,
+      response: {
+        200: AuthModel.signInResponseSchema,
+        400: AuthModel.signInFailedResponseSchema,
+      },
+    },
+  );
--- apps/primary-backend/src/modules/auth/service.ts
+++ apps/primary-backend/src/modules/auth/service.ts
@@ -1,8 +1,14 @@
 import { prisma } from "db";
 
 export abstract class AuthService {
-  static async signup(username: string, password: string): Promise<string> {
-    return "123";
+  static async signup(email: string, password: string): Promise<string> {
+    const user = await prisma.user.create({
+      data: {
+        email,
+        password,
+      },
+    });
+    return user.id.toString();
   }
 
   static async signin(username: string, password: string): Promise<string> {
